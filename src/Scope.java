07cac4a98defd0b8f0e03aa37bc4a93884be85b4
Basil Shaikh <shaikh.basil786@gmail.com>
2023-04-20 16:57:07 -0700

    Added code for scope

---
+++ src/Scope.java
@@ -0,0 +1,45 @@
+public class Scope {
+
+    static int dummy(int a, String b){
+
+        int num1=10;
+        int num2=20;
+
+        if(num1 > num2){
+            int c = 10;
+            num1 = num1 + d;
+        }
+        else if(num2 > num1){
+            int d  = 5;
+
+            num2 = num2 + d;
+        }
+        else{
+            int p = 0;
+            String[] names = new String[5];
+
+            for(int i = 0; i < 5; i++){
+                int e;
+                System.out.println(i);
+                num1  = num2 + i;
+                int x = 100;
+
+                for(int j = 0; j < x; j++){
+                    System.out.println(j);
+                }
+            }
+            //num1, num2, a, b, p, names
+        }
+
+        //num1, num2, a, b,
+
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
